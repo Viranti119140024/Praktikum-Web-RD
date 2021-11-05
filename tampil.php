e9dc6890b976262698df4e4589275ed4133839df
Viranti 119140024 <83486824+Viranti119140024@users.noreply.github.com>
2021-11-05 17:43:01 +0700

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,42 @@
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Prodi</th>
+        <th>Angkatan</th>
+        <th>Action</th>
+
+    </tr>
+    <?php
+        include "koneksi.php";
+        $hasil=mysqli_query($kon, "select * From Mahasiswa order by nim asc");
+        $no=0;
+        while($data=mysqli_fetch_array($hasil)):
+            $no++;
+    ?>
+
+    <tr>
+        <td><?php echo $no; ?></td>
+        <td><?php echo $data['nim']; ?></td>
+        <td><?php echo $data['nama']; ?></td>
+        <td><?php echo $data['prodi']; ?></td>
+        <td><?php echo $data['angkatan']; ?></td>
+        <td>
+            <button id="btn_edit" value="<?php echo $data['nim']; ?>">Edit</button>
+            <button id="btn_delete" id="deleteButton" value="<?php echo $data['nim']; ?>">Hapus</button>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+</table>
+
+<h3>Catatan : </h3> 
+<p>
+    1. NIM dari data tidak bisa diubah karena merupakan primary key
+</p> 
+<p>
+    2. Data bisa dihapus langsung dengan menekan button hapus
+</p> 
+</p> 
+    3. Data selain NIM dapat diedit
+</p>
